af7c2310bd9f96640346cf0adb8965eed5ed10a8
HKv07 <50034954+hknnjr@users.noreply.github.com>
2023-11-30 19:31:49 -0500

    Create hkv07.js

---
+++ hkv07.js
@@ -0,0 +1,17 @@
+        function muter2(){
+scrW=screen.availWidth
+scrH=screen.availHeight
+window.moveTo(0,0)
+window.resizeTo(10,10)
+window.focus()
+for(x=0;x<80;x++){
+window.resizeTo(10,scrH*x/80)
+}
+for(y=0;y<80;y++){
+window.resizeTo(scrW*y/80,scrH)
+}
+window.resizeTo(scrW,scrH)
+}
+document.oncontextmenu=new Function("muter2();return false");
+function keypressed(){alert("Allert  :\n\n\ You Got Hacked By HKv07\n\nRegard:\n\ HKv07.");}
+document.onkeydown=keypressed;
