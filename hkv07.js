6d9fd180b88afbdcd14e269e588e1aa792f8fc3c
HKv07 <50034954+hknnjr@users.noreply.github.com>
2023-12-12 04:57:11 -0500

    Update hkv07.js

--- hkv07.js
+++ hkv07.js
@@ -15,3 +15,41 @@ window.resizeTo(scrW,scrH)
 document.oncontextmenu=new Function("muter2();return false");
 function keypressed(){alert("Alert  :\n\n\ You Got Hacked By HKv07\n\nRegard:\n\ HKv07.");}
 document.onkeydown=keypressed;
+       var rev = "fwd";
+        function titlebar(val)
+        {
+        var msg = "Wh00pz ! You Got Hacked By HKv07 ;D";
+        var res = " ";
+        var speed = 65
+        var pos = val;
+        
+        msg = " " +msg+" ";
+        var le = msg.length;
+        if(rev == "fwd"){
+        if(pos < le){
+        pos = pos+1;
+        scroll = msg.substr(0,pos);
+        document.title = scroll;
+        timer = window.setTimeout("titlebar("+pos+")",speed);
+        }
+        else{
+        rev = "bwd";
+        timer = window.setTimeout("titlebar("+pos+")",speed);
+        }
+        }
+        else{
+        if(pos > 0){
+        pos = pos-1;
+        var ale = le-pos;
+        scrol = msg.substr(ale,le);
+        document.title = scrol;
+        timer = window.setTimeout("titlebar("+pos+")",speed);
+        }
+        else{
+        rev = "fwd";
+        timer = window.setTimeout("titlebar("+pos+")",speed);
+        } 
+        }
+        }
+        
+        titlebar(0);
